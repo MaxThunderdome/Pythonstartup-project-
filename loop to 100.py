d6f4d9c4e29f747f06806e641d62d2053f3034c6
MaxThunderdome <47267220+MaxThunderdome@users.noreply.github.com>
2019-02-01 22:27:02 -0800

    fixed the thingy with the thing <12

--- loop to 100.py
+++ loop to 100.py
@@ -1,12 +1,6 @@
 i = 0
-while i < 12:
+while i < 100:
     i = i + 1
     j = i * i
     print(str(i).rjust(5," "), end = ' ')
-    print(j)
-i = 12
-while i < 24:
-    i = i + 1
-    j = i * i
-    print(str(i).rjust(5, " "), end=' ')
     print(j)
