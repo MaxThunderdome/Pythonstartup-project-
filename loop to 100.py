a4cd4af879e02a5010be91f147282c907af8a17a
MaxThunderdome <47267220+MaxThunderdome@users.noreply.github.com>
2019-02-01 22:25:14 -0800

    Add files via upload

---
+++ loop to 100.py
@@ -0,0 +1,12 @@
+i = 0
+while i < 12:
+    i = i + 1
+    j = i * i
+    print(str(i).rjust(5," "), end = ' ')
+    print(j)
+i = 12
+while i < 24:
+    i = i + 1
+    j = i * i
+    print(str(i).rjust(5, " "), end=' ')
+    print(j)
